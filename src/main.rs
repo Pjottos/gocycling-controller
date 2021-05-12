905604fae007d6902701c2d601551ba506330cd1
Pjottos <35270305+Pjottos@users.noreply.github.com>
2021-05-12 12:21:08 +0200

    initial cross compiling setup

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+extern fn _start() {
+    loop {
+
+    }
+}
+
+#[panic_handler]
+fn handle_panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
