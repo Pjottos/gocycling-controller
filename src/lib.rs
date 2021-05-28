3df246aa699c92af1be54a1ee2cd6793b75c5273
Pjottos <pjotrslooff@icloud.com>
2021-05-28 11:32:30 +0200

    use pico-sdk-sys as submodule to add uart support

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,18 @@ use rpi_pico_sdk_sys::*;
 
 use core::panic::PanicInfo;
 
-
 #[no_mangle]
 pub unsafe extern "C" fn main() -> ! {
     const PIN_LED: u32 = 25;
+
+    const BAUD_RATE: u32 = 9600;
+    const TX_PIN: u32 = 0;
+    const RX_PIN: u32 = 1;
+
+    // uart_init(uart0, BAUD_RATE);
+
+    gpio_set_function(TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(RX_PIN, GPIO_FUNC_UART);
 
     gpio_init(PIN_LED);
     gpio_set_dir(PIN_LED, GPIO_OUT);
