252e3550ecc435bb8988db9ef7d3b1dc3e3d966b
Pjottos <35270305+Pjottos@users.noreply.github.com>
2021-06-01 09:18:48 +0200

    cycling progress

--- src/cycling.rs
+++ src/cycling.rs
@@ -3,5 +3,5 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct CycleData {
-    pub time: f32,
+    pub micros: u32,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![feature(asm)]
 
 use crate::cycling::CycleData;
 use core::panic::PanicInfo;
@@ -10,20 +11,37 @@ mod ctypes;
 mod host;
 mod cycling;
 
-#[no_mangle]
-pub unsafe extern "C" fn main() -> ! {
-    const PIN_LED: u32 = 25;
+const MAGNET_PIN: u32 = 2;
 
+#[no_mangle]
+pub extern "C" fn main() -> ! {
     let conn = host::Connection::new().unwrap();
-    let data = CycleData { time: 1.0 };
+
+    unsafe { init_magnet_sensor(); }
+    let mut last_cycle_time = unsafe { time_us_64() };
 
     loop {
-        conn.push(&data).unwrap();
-        sleep_ms(1000);
+        unsafe {
+            asm!("wfi");
+            let delta = time_us_64() - last_cycle_time;
+            last_cycle_time = time_us_64();
+            let data = CycleData { micros: delta as u32 };
+            conn.push(&data).unwrap();
+        }
     }
 }
 
 #[panic_handler]
 fn handle_panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+unsafe fn init_magnet_sensor() {
+    gpio_set_dir(MAGNET_PIN, GPIO_IN);
+    gpio_pull_up(MAGNET_PIN);
+    gpio_set_irq_enabled(
+        0,
+        gpio_irq_level_GPIO_IRQ_EDGE_FALL,
+        true,
+    );
 }
